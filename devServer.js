5088eb75b6c9b4ea9279815a77f1254e22fdc300
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-13 00:17:30 +0900

    update for aw

--- devServer.js
+++ devServer.js
@@ -17,11 +17,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(4000, '0.0.0.0', function(err) {
+app.listen(3001, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:4000');
+  console.log('Listening at http://localhost:3001');
 });
--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ import { projects } from 'json!../.firebaserc'
 
 export const apiKey = "AIzaSyBuSzNftbHVOOhxirAnQRqOSFIq1V9R09M"
 export const projectId = projects.default
+export const siteName = "Best news from Jon"
